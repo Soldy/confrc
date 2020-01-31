a99388ce99f600f3deffac8ddf0a9eace3552c33
Soldy <4786022+Soldy@users.noreply.github.com>
2020-01-31 15:34:04 +0000

    go to 1.0
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ const fs = require("fs");
 
 
 exports.confrc=function(){
+    /*
+     * id @string
+     * @public
+     */
+    this.get=function(id){
+        if(typeof id !== "string")
+            return false;
+        if(typeof config[id] === "undefined")
+            return undefined;
+        return config[id];
+    };
+    /*
+     * id @string
+     * @public
+     */
+    this.check=function(id){
+        if((typeof id !== "string") ||(typeof config[id] !== "undefined"))
+            return true;
+        return false;
+    }
+    /*
+     * @private
+     */
     let readDefault=function(){
         config = JSON.parse(fs.readFileSync("env.confrc.default.json").toString());
     };
+    /*
+     *@private
+     */
     let read=function(){
         try{
             let standConfig = JSON.parse(
@@ -18,17 +44,11 @@ exports.confrc=function(){
             console.log(e);
         }
     };
-    this.get=function(id){
-        if(typeof config[id] === "undefined")
-            return undefined;
-        return config[id];
-    };
-    this.check=function(id){
-        if(typeof config[id] !== "undefined")
-            return true;
-        return false;
-    }
-    var config = {};
+    /*
+     * @private
+     */
+    let config = {};
+    //costructor
     readDefault();
     read();
 };
